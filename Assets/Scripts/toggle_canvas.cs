f2e4396bd003643fe03186f87b257d68c519039e
annafoktova27 <133894133+annafoktova27@users.noreply.github.com>
2023-11-26 10:56:04 +0400

    Implementing localization

--- Assets/Scripts/toggle_canvas.cs
+++ Assets/Scripts/toggle_canvas.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class toggle_canvas : MonoBehaviour
 {
     public Canvas canvas;
+    private AudioSource audiosource;
+
+    private void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+    }
 
     private void OnMouseDown()
     {
+        audiosource.Play();   
         canvas.enabled = !canvas.enabled;
     }
 }
